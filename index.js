48c9371f57ce8cf091f84238ce33d696153e6d0e
Junaed Rayhan <junaedrayhan@gmail.com>
2021-03-30 21:03:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+require('dotenv').config()
+const port = process.env.PORT || 5055;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42gqz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+client.connect(err => {
+  const Eventcollection = client.db("volunteer").collection("events");
+//  Get Data From Database
+  app.get('/events', (req, res)=>{
+    Eventcollection.find()
+    .toArray((err, items)=>{
+      console.log('from database', items)
+      res.send(items)
+    })
+  })
+
+//  insert Data to Database
+  app.post('/addEvent', (req, res)=>{
+    const newEvent = req.body;
+    console.log('adding new event:', newEvent)
+    Eventcollection.insertOne(newEvent)
+    .then(result =>{
+      console.log('inserted Count', result)
+      res.send(result.insertedCount > 0)
+    })
+  })
+
+
+// delete with id
+  app.delete('deleteEvent/:id', (req, res) =>{
+    const id = ObjectID(req.params.id);
+    console.log('delete this', id);
+    Eventcollection.findOneAndDelete({_id: id})
+    .then(document => res.send(!!document.value))
+  })
+});
+
+
+
+
+
+app.listen(port, () => {
+  console.log('Login success')
+})
